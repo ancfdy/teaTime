75c2a8c6858cb31fc57858a9cc68ac6c56de82ca
zhangcui183fgd <1286679328@qq.com>
2017-05-19 09:27:51 +0800

    增加菜单

---
+++ TeaTime_Demo/app/src/main/java/com/teatime/teatime_demo/UI/SlideActivity.java
@@ -0,0 +1,8 @@
+package com.teatime.teatime_demo.UI;
+
+/**
+ * Created by HG on 2017/5/18.
+ */
+
+public class SlideActivity {
+}
---
+++ TeaTime_Demo/library_teaTime/src/main/java/com/teatime/library_teatime/interfaces/ScreenShotable.java
@@ -0,0 +1,12 @@
+package com.teatime.library_teatime.interfaces;
+
+import android.graphics.Bitmap;
+
+/**
+ * Created by Konstantin on 12.01.2015.
+ */
+public interface ScreenShotable {
+    public void takeScreenShot();
+
+    public Bitmap getBitmap();
+}
